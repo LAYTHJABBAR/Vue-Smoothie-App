500f64b37dd7da64a5673239e9e230c3554c2153
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2020-05-29 13:51:08 -0600

    Intiate the Vue app and design the Index Page

---
+++ src/components/Index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <h2 class="indigo-text">{{ smoothie.title}}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing,index) in smoothie.ingrident" :key="index">
+            <span class="chip">{{ing}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      smoothies: [
+        {
+          title: "Layth Drink",
+          slug: "Layth-drink",
+          ingrident: ["banana", "coffee", "Yogurt"],
+          id: "1"
+        },
+        {
+          title: "Afternoon Mode",
+          slug: "afternoon-mode",
+          ingrident: ["banana", "coffee", "hot souce"],
+          id: "2"
+        },
+        {
+          title: "Morning Mode",
+          slug: "morning-mode",
+          ingrident: ["coffee"],
+          id: "3"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.index{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin-top: 40px;
+  grid-gap: 20px
+}
+.index .ingredients{
+  margin: 30px auto;
+}
+.index .ingredients li{
+  display: inline-block;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Smoothie</span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab pink">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navebar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+#navbar nav{
+    padding: 0 20px;
+}
+</style>
