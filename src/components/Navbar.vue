327d42733f2fbd7abf8d4e572de91cdc23ee4397
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2020-05-30 10:51:50 -0600

    Add the Delete ,Edit, Add Smoothier components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,11 @@
   <div id="navbar">
     <nav class="nav-extended indigo darken-2">
       <div class="nav-content">
-        <router-link to="">
+        <router-link :to="{ name: 'Index'}">
           <span class="nav-title">Smoothie</span>
         </router-link>
         <a href="" class="btn-floating btn-large halfway-fab pink">
-          <router-link to="">
+          <router-link :to="{name: 'AddSmoothie'}">
             <i class="material-icons">add</i>
           </router-link>
         </a>
